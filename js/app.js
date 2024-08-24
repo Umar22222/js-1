95361f06c83a2964285bc696710b8c7066ca6512
Umar22222 <asadovumar222@gmail.com>
2024-08-25 02:37:59 +0500

    .

---
+++ js/app.js
@@ -0,0 +1,119 @@
+{
+    // 1 - misol
+    // let a = +prompt("A sonini ktiting")
+
+    // let b = 4
+
+    // console.log(`A ni perimetri =  ${a * b} SM`);
+    
+}
+
+{
+    // 2 - misol
+    // let a = prompt("A sonini ktiting")
+
+    // let s = a * a
+    // console.log(`A ni kvadrati = ${s}`);
+    
+}
+
+{
+    // 3 - misol
+    // let a = +prompt("A sonini ktiting")
+    // let b = +prompt("B sonini ktiting")
+
+    // let s = a * b
+    // let p = 2 * (a + b)
+
+    // console.log(`A ni perimetri = ${p} SM`);
+    
+}
+
+{
+    // 4 - misol
+    // let d = +prompt("D sonini ktiting")
+
+    // let p = 3.14
+    // let l = p * d
+
+    // console.log(`A ni uzunligi = ${l} SM`);
+    
+}
+
+{
+    // 5 - misol
+    // let a = +prompt("A sonini ktiting")
+
+    // let v = a * 3 * 3
+    // let b = a * a
+    // let s = 6 * b 
+
+    // console.log(`A ni hajmi = ${v} L`);
+    // console.log(`A ni sirti ${s} S`);
+    
+}
+
+{
+    // 6 - misoll
+    // let a = +prompt("A sonini ktiting")
+
+    // let b = +prompt("B sonini ktiting")
+    // let c = +prompt("C sonini ktiting")
+    // let v = a * b * c
+    // let s = 2 * (a * b + b * c + a * c)
+
+    // console.log(`hajmi = ${v} L`);
+    // console.log(`sirti = ${s} S`);
+       
+}
+
+{
+    // 7 - misol
+    // let r = +prompt("R sonini kiriting")  
+
+    // let rr = r * r
+    // let p = 3.14
+    // let l = 2 * p * r 
+    // let s = p * rr
+
+    // console.log(`Doiraning uzunligi =  ${l} SM`);
+    // console.log(`Doiraning yuzasi =  ${s} SM`);
+    
+}
+
+{
+    // 8 - misol
+    // let a = +prompt(`A sonini kiriting`)
+    // let b = +prompt(`B sonini kiriting`)
+
+    // console.log(`o'rta arefmetikasi =  ${(a + b) / 2} teng`);
+    
+}
+
+{
+    // 9 - misol
+    // let a = prompt(`A sonini kiriting`)
+    // let b = prompt(`B sonini kiriting`)
+
+    // let c = a * b
+
+    // console.log(Math.sqrt(c));
+     
+}
+
+{
+    // 10 - misol
+    // let a = +prompt(`A sonini kiriting`)
+    // let b = +prompt(`B sonini kiriting`)
+
+    // let c = a + b
+    // let e = a * b
+    // let f = a * a
+    // let g = b * b
+    
+    // console.log(`sonlarning yeg'indisi = ${c}`);
+    // console.log(`sonlarning ko'paytmasi = ${e}`);
+    // console.log(`A aonin kvadrati = ${f}`);
+    // console.log(`B aonin kvadrati = ${g}`);
+    
+}
